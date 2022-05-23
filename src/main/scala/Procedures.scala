187b54d24913368facf1f042c98296b92423f9b0
Alonso Montero <lumontero@mobilize.net>
2022-05-23 10:06:52 -0600

    Add useful comments

--- src/main/scala/Procedures.scala
+++ src/main/scala/Procedures.scala
@@ -6,6 +6,15 @@ import com.snowflake.snowpark.types.StructField
 import com.snowflake.snowpark.types.IntegerType
 import com.snowflake.snowpark.types.StringType
 object Example {
+    /* You can create a Snowflake stored procedure from the 'showDummyTable' function. You only need to follow these steps:
+        1. Import the build (this can be done from the Metals extension tab on the left activity bar). Wait for this import to finish.
+        2. Right-click on the build.sbt file in the root of this project.
+        3. Choose the 'Black Diamond: Export Scala UDF' option.
+        4. Fill the template file 'UDF-Export-Snowflake-Scala.sql' (on the 'Target' folder) with the requested information
+            (you can ignore or delete the section 3.2, since that part is used to export a User Defined Function and section 3.1
+            is used to export a Procedure, which is what we are doing in this case).
+        5. Execute the SQL commands in the template file.
+    */
     def showDummyTable(sess: Session): String = {
         val idColumn: StructField = StructField("id", IntegerType)
         val valueColumn: StructField = StructField("value", StringType)
--- src/main/scala/UserDefinedFunctions.scala
+++ src/main/scala/UserDefinedFunctions.scala
@@ -1,6 +1,15 @@
 package UserDefinedFunctions
 
 object Math {
+    /* You can create a Snowflake UDF (User Defined Function) from the 'simpleDivision' function. You only need to follow these steps:
+        1. Import the build (this can be done from the Metals extension tab on the left activity bar). Wait for this import to finish.
+        2. Right-click on the build.sbt file in the root of this project.
+        3. Choose the 'Black Diamond: Export Scala UDF' option.
+        4. Fill the template file 'UDF-Export-Snowflake-Scala.sql' (on the 'Target' folder) with the requested information
+            (you can ignore or delete the section 3.1, since that part is used to export a Procedure and section 3.2
+            is used to export a User Defined Function, which is what we are doing in this case).
+        5. Execute the SQL commands in the template file.
+    */
     def simpleDivision(a: Double, b: Double): Double = {
         a / b
     }
